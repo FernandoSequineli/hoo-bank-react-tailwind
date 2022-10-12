afc01eb8d2008bf6f6356946a67793b2b3f1a7bd
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-10-12 09:44:46 +0100

    created components structure for the application

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import styles from "./style";
+import {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+} from "./components";
 
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>Navbar</div>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
