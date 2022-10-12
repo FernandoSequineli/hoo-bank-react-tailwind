d4e4316a447ecae3be4ee8a8c09f5ae00de1e8cf
Fernando Sequineli <sequineli.fernando@gmail.com>
2022-10-12 09:32:29 +0100

    added style js, constants and configured Tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
-function App() {
-  return <div>Hello</div>;
-}
+import styles from "./style";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Web elements :D</div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
